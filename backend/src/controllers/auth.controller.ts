da3b3b76ffe509d82b15238849db3c0044a0f591
SULLENGOD <luis.d.sullen@outlook.com>
2023-12-19 17:56:50 -0600

    add doc comments

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,8 +1,19 @@
+
 import { Request, Response } from "express";
 import User, { IUser } from "../models/user.model";
 import jwt from "jsonwebtoken";
 import { validationResult } from "express-validator";
 
+/**
+ * Registers a new user.
+ *
+ * @param {Request} req Express request object containing user data (username, email, password).
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response with saved user data (excluding password) on success, or error message on failure.
+ *
+ * @throws {400 Bad Request} If validation errors occur.
+ * @throws {500 Internal Server Error} If any unexpected error occurs during signup.
+ */
 export const signup = async (req: Request, res: Response) => {
   try {
     const errors = validationResult(req.body);
@@ -30,6 +41,17 @@ export const signup = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Authenticates an existing user and generates a JWT token.
+ *
+ * @param {Request} req Express request object containing login credentials (email, password).
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response with user data and JWT token on success, or error message on failure.
+ *
+ * @throws {400 Bad Request} If validation errors occur or email is wrong.
+ * @throws {400 Bad Request} If password is incorrect.
+ * @throws {500 Internal Server Error} If any unexpected error occurs during login.
+ */
 export const signin = async (req: Request, res: Response) => {
   try {
     const errors = validationResult(req.body);
@@ -59,6 +81,17 @@ export const signin = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Retrieves the currently logged-in user's profile information.
+ *
+ * @param {Request} req Express request object with user ID in headers or body.
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response with user data on success, or error message on failure.
+ *
+ * @throws {400 Bad Request} If validation errors occur.
+ * @throws {404 Not Found} If the user with the provided ID is not found.
+ * @throws {500 Internal Server Error} If any unexpected error occurs while fetching profile.
+ */
 export const profile = async (req: Request, res: Response) => {
   try {
     const errors = validationResult(req.body);
@@ -75,6 +108,17 @@ export const profile = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Retrieves the username of a specific user.
+ *
+ * @param {Request} req Express request object with user ID in URL parameters.
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response with the user's username on success, or error message on failure.
+ *
+ * @throws {400 Bad Request} If validation errors occur.
+ * @throws {404 Not Found} If the user with the provided ID is not found.
+ * @throws {500 Internal Server Error} If any unexpected error occurs while fetching username.
+ */
 export const profileName = async (req: Request, res: Response) => {
   try {
     const errors = validationResult(req.body);
@@ -91,6 +135,18 @@ export const profileName = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Changes the role of another user (requires super_administrator privileges).
+ *
+ * @param {Request} req Express request object with user ID and new role in body.
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response with a success message and updated username and role on success, or error message on failure.
+ *
+ * @throws {400 Bad Request} If validation errors occur.
+ * @throws {401 Unauthorized} If the logged-in user is not a super_administrator.
+ * @throws {404 Not Found} If the user with the provided ID is not found.
+ * @throws {500 Internal Server Error} If any unexpected error occurs while changing role.
+ */
 export const changeRole = async (req: Request, res: Response) => {
   try {
     const errors = validationResult(req.body);
@@ -113,6 +169,18 @@ export const changeRole = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Promotes a user to moderator role.
+ *
+ * @param {Request} req Express request object containing the user ID to be promoted.
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response with a success message and updated username on success, or error message on failure.
+ *
+ * @throws {400 Bad Request} If validation errors occur in the request body.
+ * @throws {401 Unauthorized} If the logged-in user is not authorized to promote (requires super_administrator or administrator role).
+ * @throws {404 Not Found} If the user to be promoted is not found.
+ * @throws {500 Internal Server Error} If any unexpected error occurs while updating the user role.
+ */
 export const createModerator = async (req: Request, res: Response) => {
   try {
     const errors = validationResult(req.body);
@@ -137,6 +205,18 @@ export const createModerator = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Mutes a specific user.
+ *
+ * @param {Request} req Express request object containing the user ID to be muted.
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response with a success message and username on success, or error message on failure.
+ *
+ * @throws {400 Bad Request} If validation errors occur in the request body.
+ * @throws {401 Unauthorized} If the logged-in user is not authorized to mute (requires super_administrator or administrator role).
+ * @throws {404 Not Found} If the user to be muted is not found.
+ * @throws {500 Internal Server Error} If any unexpected error occurs while updating the user's muted flag.
+ */
 export const muteAuthor = async (req: Request, res: Response) => {
   try {
     const errors = validationResult(req.body);
--- backend/src/controllers/posts.controller.ts
+++ backend/src/controllers/posts.controller.ts
@@ -2,6 +2,16 @@ import { Request, Response } from "express";
 import Post, { IPost } from "../models/post.model";
 import User from "../models/user.model";
 
+/**
+ * Creates a new post by the currently logged-in user.
+ *
+ * @param {Request} req Express request object containing post data (title, content, tags).
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response containing the saved post and updated user information (excluding password) on success, or error message on failure.
+ *
+ * @throws {401 Unauthorized} If the logged-in user is muted or unauthorized.
+ * @throws {500 Internal Server Error} If any unexpected error occurs while creating the post.
+ */
 export const newPost = async (req: Request, res: Response) => {
   try {
     const user = await User.findById(req.userId, { password: 0 });
@@ -28,6 +38,17 @@ export const newPost = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Retrieves a specific post by its ID.
+ *
+ * @param {Request} req Express request object containing the post ID in URL parameters.
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response containing the requested post details on success, or error message on failure.
+ *
+ * @throws {404 Not Found} If the post with the provided ID is not found.
+ * @throws {500 Internal Server Error} If any unexpected error occurs while fetching the post.
+ */
+
 export const getPost = async (req: Request, res: Response) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -40,10 +61,21 @@ export const getPost = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Retrieves all existing posts.
+ *
+ * @param {Request} req Express request object.
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response containing a list of all posts on success, or error message on failure.
+ *
+ * @throws {404 Not Found} If no posts are found.
+ * @throws {500 Internal Server Error} If any unexpected error occurs while fetching posts.
+ */
 export const getAllPosts = async (req: Request, res: Response) => {
   try {
     const posts = await Post.find();
-    if (!posts || posts.length === 0) return res.status(404).json("Posts not found");
+    if (!posts || posts.length === 0)
+      return res.status(404).json("Posts not found");
 
     res.json(posts);
   } catch (error) {
@@ -52,6 +84,16 @@ export const getAllPosts = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Retrieves a limited number of latest posts.
+ *
+ * @param {Request} req Express request object containing the desired number of posts in URL parameters.
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response containing a list of the requested number of posts on success, or an empty response on invalid limit.
+ *
+ * @throws {418 I'm a teapot} If the provided limit is invalid.
+ * @throws {500 Internal Server Error} If any unexpected error occurs while fetching posts.
+ */
 export const getPosts = async (req: Request, res: Response) => {
   try {
     const limit = parseInt(req.params.limit);
@@ -65,12 +107,24 @@ export const getPosts = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Searches for posts matching a specific keyword in their title.
+ *
+ * @param {Request} req Express request object containing the search keyword in URL parameters.
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response containing a list of matching posts on success, or an empty response on an empty query.
+ *
+ * @throws {418 I'm a teapot} If the provided query is empty.
+ * @throws {500 Internal Server Error} If any unexpected error occurs while searching posts.
+ */
 export const getSearch = async (req: Request, res: Response) => {
   try {
     const query = req.params.search;
     if (!query) return res.status(418).json();
 
-    const posts = await Post.find({ title: { $regex: new RegExp(query, 'i') } });
+    const posts = await Post.find({
+      title: { $regex: new RegExp(query, "i") },
+    });
     res.json(posts);
   } catch (error) {
     console.error("Error searching posts:", error);
@@ -78,6 +132,16 @@ export const getSearch = async (req: Request, res: Response) => {
   }
 };
 
+/*
+ * Deletes a specific post and removes its reference from the author's list.
+ *
+ * @param {Request} req Express request object containing the post ID in URL parameters.
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response containing an updated list of the logged-in user's posts on success, or error message on failure.
+ *
+ * @throws {401 Unauthorized} If the logged-in user is not the post author.
+ * @throws {500 Internal Server Error} If any unexpected error occurs while deleting the post.
+ */
 export const deletePost = async (req: Request, res: Response) => {
   try {
     const user = await User.findById(req.userId, { password: 0 });
@@ -101,6 +165,17 @@ export const deletePost = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * Updates an existing post with the provided data, only allowing modifications by the post author.
+ *
+ * @param {Request} req Express request object containing the post data (ID, title, content, tags) for update.
+ * @param {Response} res Express response object.
+ * @returns {Response} JSON response containing the updated post details on success, or error message on failure.
+ *
+ * @throws {401 Unauthorized} If the logged-in user is not the post author.
+ * @throws {404 Not Found} If the post with the provided ID is not found.
+ * @throws {500 Internal Server Error} If any unexpected error occurs while updating the post.
+ */
 export const updatePost = async (req: Request, res: Response) => {
   try {
     const user = await User.findById(req.userId, { password: 0 });
@@ -114,7 +189,11 @@ export const updatePost = async (req: Request, res: Response) => {
       tags: req.body.tags,
     };
 
-    const renewedPost = await Post.findOneAndUpdate({ _id: req.body._id }, updatedPostData, { new: true });
+    const renewedPost = await Post.findOneAndUpdate(
+      { _id: req.body._id },
+      updatedPostData,
+      { new: true }
+    );
 
     res.json(renewedPost);
   } catch (error) {
