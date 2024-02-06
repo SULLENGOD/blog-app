87bd11043fd0857bb841babb2e0430540115f031
Luis Martinez (Sullen) <luis.d.sullen@outlook.com>
2024-02-06 11:14:16 -0600

    change navbar styles

--- frontend/src/layout/Navbar/Navbar.tsx
+++ frontend/src/layout/Navbar/Navbar.tsx
@@ -6,8 +6,8 @@ const Navbar = () => {
 
   return (
     <header className="flex justify-center gap-1 border-b border-white-line">
-      <img src={EyeLogo} alt="Eye" className="eye-logo" />
-      <nav className="flex gap-3">
+      <img src={EyeLogo} alt="Eye" className="eye-logo m-2 size-9"/>
+      <nav className="flex gap-5">
         <NavLink to="/" className="text-white-paper text-2xl hover:text-white-paper-50 transition-all self-center">Home</NavLink>
         <NavLink to="/search" className="text-white-paper text-2xl hover:text-white-paper-50 transition-all self-center">Search</NavLink>
         <NavLink to="/account/login" className="text-white-paper text-2xl hover:text-white-paper-50 transition-all self-center">Profile</NavLink>
